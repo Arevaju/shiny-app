ee5e89ea9c5a011e224ef428f9e4ff433e754c5e
unknown <torreju@D01RI1000323.ies.jrc.it>
2014-06-23 09:23:14 +0200

    First version

---
+++ server.r
@@ -0,0 +1,40 @@
+shinyServer(function(input, output) { 
+  passData <- reactive({
+    data <- data[data$Country %in% input$country_filter1 & 
+                   data$Year %in% input$year,]
+    rownames(data) = unique(data$NUTS_CODE)  
+    data
+  })
+  
+  passData1 <- reactive({
+    data <- data[data$Country %in% input$country_filter1,]
+    datasplit <- split(data[7:9], data$Year, drop=TRUE)
+    datasplit <- do.call(cbind, datasplit)
+    rownames(datasplit) = unique(datasplit$NUTS_CODE)  
+    datasplit
+  })
+  
+  ### Radar chart output 1  
+  output$radar <- renderChart2({
+    a<- rCharts:::Highcharts$new() 
+    a$chart(type='line',polar=TRUE, width=600,height = 350)
+    a$pane(size='90%')
+    a$title(text= "Title")
+    a$xAxis(categories= rownames(passData()),tickmarkPlacement='on',lineWidth=0)
+    a$yAxis(gridLineInterpolation='polygon',lineWidth=0,min=0)
+    a$data(passData()[input$indicator],pointPlacement='on')    
+    return(a)    
+  })
+  
+  #### Radar chart output 2
+  output$radar1 <- renderChart2({
+    a<- rCharts:::Highcharts$new() 
+    a$chart(type='line',polar=TRUE, width=600,height = 350)
+    a$pane(size='90%')
+    a$title(text= "Title")
+    a$xAxis(categories= rownames(passData()),tickmarkPlacement='on',lineWidth=0)
+    a$yAxis(gridLineInterpolation='polygon',lineWidth=0,min=0)
+    a$data(passData1(),pointPlacement='on')    
+    return(a)    
+  })
+})
---
+++ ui.r
@@ -0,0 +1,32 @@
+library("shiny")
+library("rCharts")
+shinyUI(pageWithSidebar(  
+  headerPanel( "Shiny App"), 
+  sidebarPanel(
+    selectInput(inputId = "indicator",
+                label = (HTML("<b>Select a list of indicators:</b>")),
+                 multiple = TRUE,
+                 selected = "Ind1",
+             #  choices = list("Water stress" = "Ind1,Ind2"))),
+               as.list(colnames(data)[7:9])),    
+          
+    checkboxGroupInput(inputId = 'country_filter1',
+                       label = (HTML("<b>Select the region of interest:</b>")), 
+                       sort(unique(data$Country)),
+                       selected = "Austria"),
+    radioButtons(inputId ="year",
+                 label = (HTML("<b>Select the year of interest:</b>")),
+                 choices = list ("2010"= "2010",
+                                 "2020" = "2020",
+                                 "2050" = "2050"
+                 )
+    ),
+    submitButton(text="Run", icon("refresh"))  
+  ),
+  # Show the results from the query  
+  mainPanel(  
+    tabsetPanel(
+    tabPanel("Charts",showOutput ("radar", "Highcharts")),   
+    tabPanel("Multiyear", showOutput ("radar1", "Highcharts"))
+  )
+)))
